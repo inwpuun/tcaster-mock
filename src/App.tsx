200ac47755ef96197ac120b9ab3dea670689a869
inwpuun <112232858+inwpuun@users.noreply.github.com>
2024-03-12 14:30:21 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+
+export function App() {
+  return (
+    <div className='flex w-10 h-10 text-green-600'>
+      hello world
+    </div>
+  )
+}
+
---
+++ src/components/TcasterCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export function TcasterCard({}: Props) {
+  return (
+    <div>TcasterCard</div>
+  )
+}
